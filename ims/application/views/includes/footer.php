00bd8eb250e29066ed3df28c918fdeef8016818b
rajesh2006 <sulthan342@gmail.com>
2014-10-16 20:20:33 -0500

    fgdfgd

---
+++ ims/application/views/includes/footer.php
@@ -0,0 +1,20 @@
+
+<DOCTYPE html>
+    <html>
+        <head>
+            
+      
+            </head>
+            <body>
+                <div class="navbar navbar-inverse navbar-fixed-bottom">
+                    <div class="container">
+                        <p class="navbar-text pull-left">@Copyright Dr.Box & Team</p>
+                        <a href="http://www.chemtura.com" class ="navbar-button btn-danger btn pull-right"> Chemtura home page</a>
+                        
+                    </div>
+                </div>
+                
+                </body>
+             
+            
+    </html>
